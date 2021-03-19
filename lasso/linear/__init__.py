5a9a751ac57a908d650b59b144876713b632351b
rfeinman <rfeinman16@gmail.com>
2021-03-19 19:34:23 -0400

    new init file for linear

---
+++ lasso/linear/__init__.py
@@ -0,0 +1,5 @@
+from . import solvers
+from . import utils
+from .dict_learning import (dict_learning, dict_evaluate, update_dict,
+                            update_dict_ridge)
+from .sparse_encode import sparse_encode
