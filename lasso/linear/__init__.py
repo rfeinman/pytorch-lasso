dce934cad44794738817e8b20c108e449d3922b5
rfeinman <rfeinman16@gmail.com>
2021-03-23 20:58:44 -0400

    adding new reusable function initialize_code

--- lasso/linear/__init__.py
+++ lasso/linear/__init__.py
@@ -2,4 +2,4 @@ from . import solvers
 from . import utils
 from .dict_learning import (dict_learning, dict_evaluate, update_dict,
                             update_dict_ridge)
-from .sparse_encode import sparse_encode
+from .sparse_encode import sparse_encode, initialize_code
--- lasso/linear/sparse_encode.py
+++ lasso/linear/sparse_encode.py
@@ -13,30 +13,42 @@ _init_defaults = {
     'interior-point': 'ridge'
 }
 
+
+def initialize_code(x, weight, alpha, mode):
+    n_samples = x.size(0)
+    n_components = weight.size(1)
+    if mode == 'zero':
+        z0 = x.new_zeros(n_samples, n_components)
+    elif mode == 'unif':
+        z0 = x.new(n_samples, n_components).uniform_(-0.1, 0.1)
+    elif mode == 'lstsq':
+        z0 = lstsq(x.T, weight).T
+    elif mode == 'ridge':
+        z0 = ridge(x.T, weight, alpha=alpha).T
+    elif mode == 'transpose':
+        z0 = torch.matmul(x, weight)
+    else:
+        raise ValueError("invalid init parameter '{}'.".format(mode))
+
+    return z0
+
+
 def sparse_encode(x, weight, alpha=1.0, z0=None, algorithm='ista', init=None,
                   **kwargs):
     n_samples = x.size(0)
     n_components = weight.size(1)
-    if init is None:
-        init = _init_defaults.get(algorithm, 'zero')
 
+    # initialize code variable
     if z0 is not None:
         assert z0.shape == (n_samples, n_components)
-    elif init == 'zero':
-        if algorithm == 'iter-ridge':
-            warnings.warn("Iterative Ridge should not be zero-initialized.")
-        z0 = x.new_zeros(n_samples, n_components)
-    elif init == 'unif':
-        z0 = x.new(n_samples, n_components).uniform_(-0.1, 0.1)
-    elif init == 'lstsq':
-        z0 = lstsq(x.T, weight).T
-    elif init == 'ridge':
-        z0 = ridge(x.T, weight, alpha=alpha).T
-    elif init == 'transpose':
-        z0 = torch.matmul(x, weight)
     else:
-        raise ValueError("invalid init parameter '{}'.".format(init))
+        if init is None:
+            init = _init_defaults.get(algorithm, 'zero')
+        elif init == 'zero' and algorithm == 'iter-ridge':
+            warnings.warn("Iterative Ridge should not be zero-initialized.")
+        z0 = initialize_code(x, weight, alpha, mode=init)
 
+    # perform inference
     if algorithm == 'cd':
         z = coord_descent(x, weight, z0, alpha, **kwargs)
     elif algorithm == 'gpsr':
