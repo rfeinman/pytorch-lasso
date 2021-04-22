5df74ed26fa053cc417b6fff2f5dadb377e5dc93
rfeinman <rfeinman16@gmail.com>
2021-04-22 12:23:16 -0400

    refactor iterative ridge: only update the non-zero parameters at each iteration

--- lasso/linear/solvers/iterative_ridge.py
+++ lasso/linear/solvers/iterative_ridge.py
@@ -1,3 +1,4 @@
+import warnings
 from torch import Tensor
 import torch
 from scipy.optimize import minimize_scalar
@@ -7,8 +8,8 @@ from ...conjgrad import conjgrad
 from ..utils import batch_cholesky_solve
 
 
-def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-5, eps=None,
-                    maxiter=100, line_search=True, cg=False, cg_options=None,
+def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-4, eps=None,
+                    maxiter=20, line_search=True, cg=False, cg_options=None,
                     verbose=False):
     """Iterated Ridge Regression method for Lasso problems
 
@@ -34,7 +35,7 @@ def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-5, eps=None,
     maxiter : int, optional
         Maximum number of iterations
     line_search : bool
-        Whether to use strong-wolfe line search (as opposed to fixed step size)
+        Whether to use line search optimization (as opposed to fixed step size)
     cg : bool
         Whether to use conjugate gradient to solve the ridge problem at each
         iteration. When `False` (default), Cholesky factorization is used.
@@ -45,12 +46,13 @@ def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-5, eps=None,
 
     Returns
     -------
-    zk : Tensor
+    z : Tensor
         Optimize code vectors
-    success : bool
-        Boolean indicating if the optimization succeeded
 
     """
+    if tikhonov < 1e-5:
+        warnings.warn('small regularization value %0.4e may lead to '
+                      'inprecise results.' % tikhonov)
     if cg and cg_options is None:
         cg_options = {}
     if eps is None:
@@ -71,30 +73,34 @@ def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-5, eps=None,
     # right hand side of the residual sum of squares (RSS) problem. [B,D]
     rhs = torch.mm(x, weight)  # [B,D] = [B,K] @ [K,D]
 
+    # batch gram matrix W^T @ W. [D,D]
+    A = torch.mm(weight.T, weight)
+
     if not cg:
-        # batch gram matrix W^T @ W. [B,D,D]
-        A = torch.mm(weight.T, weight).expand(z.size(0), -1, -1)
+        A = A.expand(z.size(0), -1, -1)  # [B,D,D]
 
     for k in range(1, maxiter + 1):
-        # compute ridge diagonal factor
+        # compute diagonal factor
         zmag = z.abs()
-        zmag_inv = zmag.reciprocal().masked_fill(zmag < eps, 0)
+        is_zero = zmag < eps
+        diag = (alpha / zmag).masked_fill(is_zero, 0)
+        rhs_k = rhs.masked_fill(is_zero, 0.)
 
-        # solve ridge problem
+        # solve ridge sub-problem
         if cg:
             # use conjugate gradient method
             def Adot(v):
-                Av = torch.mm(torch.mm(v, weight.T), weight)
-                Av += (2 * alpha * zmag_inv + tikhonov) * v
+                Av = torch.mm(v.masked_fill(is_zero, 0.), A)
+                Av.masked_fill_(is_zero, 0.)
+                Av.addcmul_(diag + tikhonov, v)
                 return Av
             dot = lambda u, v: torch.sum(u*v, 1, keepdim=True)
-            z_sol = conjgrad(rhs, Adot, dot, **cg_options)
+            z_sol = conjgrad(rhs_k, Adot, dot, **cg_options)
         else:
             # use cholesky factorization
-            Ak = A + 2 * alpha * torch.diag_embed(zmag_inv)  # [B,D,D]
-            if tikhonov > 0:
-                Ak.diagonal(dim1=1, dim2=2).add_(tikhonov)
-            z_sol = batch_cholesky_solve(rhs, Ak)  # [B,D]
+            A_k = A.masked_fill((is_zero.unsqueeze(1) | is_zero.unsqueeze(2)), 0.)
+            A_k.diagonal(dim1=1, dim2=2).add_(diag + tikhonov)
+            z_sol = batch_cholesky_solve(rhs_k, A_k)  # [B,D]
 
         if line_search:
             # line search optimization
@@ -104,11 +110,11 @@ def iterative_ridge(z0, x, weight, alpha=1.0, tol=1e-5, tikhonov=1e-5, eps=None,
             t = res.x
             fval = torch.tensor(res.fun)
             update = p.mul(t)
-            z = z + update
+            z = torch.where(is_zero, z, z + update)
         else:
             # fixed step size
             update = z_sol - z
-            z = z_sol
+            z = torch.where(is_zero, z, z_sol)
             fval = f(z)
 
         if verbose:
