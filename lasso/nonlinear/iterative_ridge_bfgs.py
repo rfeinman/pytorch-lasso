8a09a6f774fd9bb32db1e11ab42c038e75cf71ba
rfeinman <rfeinman16@gmail.com>
2021-04-22 18:42:10 -0400

    significant refactor of iterative_ridge_bfgs. remove iterative_ridge_ncg until later refactor

--- lasso/nonlinear/iterative_ridge_bfgs.py
+++ lasso/nonlinear/iterative_ridge_bfgs.py
@@ -1,24 +1,45 @@
-import warnings
-from scipy.optimize import OptimizeResult
-from scipy.optimize.optimize import _status_message
 from torch import Tensor
 import torch
 import torch.autograd as autograd
-from torch.optim.lbfgs import _strong_wolfe
+from scipy.optimize import minimize_scalar
 
 from ..linear.utils import batch_cholesky_solve
 
-Inf = float('inf')
+outer = lambda u,v: u.unsqueeze(-1) * v.unsqueeze(-2)
+inner = lambda u,v: torch.sum(u*v, 1, keepdim=True)
 
 
-def pinv(x, eps=1e-8):
-    return x.reciprocal().masked_fill(x < eps, 0)
+class BFGS(object):
+    def __init__(self, x, g):
+        self.H = torch.diag_embed(torch.ones_like(x))  # [B,D,D]
+        self.x_prev = x.clone(memory_format=torch.contiguous_format)
+        self.g_prev = g.clone(memory_format=torch.contiguous_format)
+
+    def update(self, x, g):
+        s = x - self.x_prev
+        y = g - self.g_prev
+        # update the BFGS hessian approximation
+        rho_inv = inner(y, s)
+        valid = rho_inv.abs().gt(1e-10)
+        rho = torch.where(valid,
+                          rho_inv.reciprocal(),
+                          torch.full_like(rho_inv, 1000.))
+
+        HssH = torch.bmm(self.H, torch.bmm(outer(s, s), self.H.transpose(1,2)))
+        sHs = inner(s, torch.bmm(self.H, s.unsqueeze(2)).squeeze(2))
+        self.H = torch.where(
+            valid.unsqueeze(2),
+            self.H + rho.unsqueeze(2) * outer(y, y) - HssH / sHs.unsqueeze(2),
+            self.H
+        )
+        self.x_prev.copy_(x, non_blocking=True)
+        self.g_prev.copy_(g, non_blocking=True)
 
 
 @torch.no_grad()
-def iterative_ridge_bfgs(f, x0, alpha=1.0, gtol=1e-5, lr=1.0, lambd=1e-4,
-                         line_search=True, normp=Inf, maxiter=None,
-                         return_losses=False, disp=False):
+def iterative_ridge_bfgs(f, x0, alpha=1.0, lr=1.0, xtol=1e-5, tikhonov=1e-4,
+                         eps=None, line_search=True, maxiter=None,
+                         verbose=0):
     """A BFGS analogue to Iterative Ridge for nonlinear reconstruction terms.
 
     Parameters
@@ -27,151 +48,87 @@ def iterative_ridge_bfgs(f, x0, alpha=1.0, gtol=1e-5, lr=1.0, lambd=1e-4,
         Scalar objective function to minimize
     x0 : Tensor
         Initialization point
-    gtol : float
-        Gradient norm must be less than `gtol` before successful
-        termination.
+    alpha : float
+        Sparsity weight of the Lasso problem
     lr : float
         Initial step size (learning rate) for each line search.
+    xtol : float
+        Convergence tolerance on changes to parameter x
+    eps : float
+        Threshold for non-zero identification
     line_search : bool
-        Whether or not to perform line search for optimal step size.
-    normp : int, inf
-        The norm type to use for calculating gradient magnitude.
+        Whether to use line search optimization (as opposed to fixed step size)
     maxiter : int, optional
         Maximum number of iterations to perform. Defaults to 200 * num_params
-    disp : bool
-        Set to True to print convergence messages.
+    verbose : int
+        Verbosity level
+
     """
     assert x0.dim() == 2
-    xshape = x0.shape
-    x = x0.detach()
     if maxiter is None:
-        maxiter = x.size(1) * 200
-
-
-    def terminate(warnflag, msg):
-        if disp:
-            print(msg)
-            print("         Current function value: %f" % Fval)
-            print("         Iterations: %d" % k)
-            print("         Function evaluations: %d" % nfev)
-        result = OptimizeResult(fun=Fval, jac=grad, nfev=nfev,
-                                status=warnflag, success=(warnflag==0),
-                                message=msg, x=x, nit=k)
-        if return_losses:
-            return result, losses
-        return result
+        maxiter = x0.size(1) * 5
+    if eps is None:
+        eps = torch.finfo(x0.dtype).eps
+    verbose = int(verbose)
 
     def evaluate(x):
         x = x.detach().requires_grad_(True)
         with torch.enable_grad():
             fval = f(x)
+        # NOTE: do not include l1 penalty term in the gradient
         grad, = autograd.grad(fval, x)
-        # add tikhonov regularization (still on f)
-        if lambd > 0:
-            fval = fval + 0.5 * lambd * x.pow(2).sum()
-            grad = grad + lambd * x
-        # add L1 regularization (now we are on F)
-        if alpha == 0:
-            Fval, gradF = fval, grad
-        else:
-            Fval = fval + alpha * x.abs().sum()
-            gradF = grad + alpha * x.sign()
-        return fval.detach(), Fval.detach(), grad, gradF
+        fval = fval.detach() + alpha * x.norm(p=1)
+        return fval, grad
 
-    def dir_evaluate(x, t, d):
-        """used for strong-wolfe line search"""
-        x = (x + t * d).reshape(xshape)
-        fval, Fval, grad, gradF = evaluate(x)
-        return Fval, gradF.flatten()
+    # initialize
+    x = x0.detach()
+    fval, grad = evaluate(x)
+    if verbose:
+        print('initial loss: %0.4f' % fval)
+    t = torch.clamp(lr / grad.norm(p=1), max=lr)
+    delta_x = x.new_tensor(float('inf'))
+    bfgs = BFGS(x, grad)
 
-    # compute initial f(x) and f'(x)
-    _, Fval, grad, gradF = evaluate(x)
-    gradF_norm = gradF.norm(normp)
-    nfev = 1
-    if disp > 1:
-        print('initial loss: %0.4f' % Fval)
-    if return_losses:
-        losses = [Fval.item()]
-
-    # initialize BFGS
-    H = torch.diag_embed(torch.ones_like(x))  # [B,D,D]
-
-    # BFGS iterations
+    # begin main loop
     for k in range(1, maxiter + 1):
-        # set the initial step size
-        if k == 1:
-            # use sample-specific learning rate for the first step,
-            # unless we're doing a line search.
-            t = (lr / gradF.abs().sum(1, keepdim=True)).clamp(None, lr)
-            if line_search:
-                t = t.mean().item()
-        else:
-            t = lr
+        # locate zeros
+        xmag = x.abs()
+        is_zero = xmag < eps
 
-        # compute newton direction
-        if k == 1:
-            # use - grad_F for the first step
-            d = gradF.neg()
-        else:
-            # use - H^{-1} @ grad_F for remaining steps
-            Hk = H
-            if alpha > 0:
-                Hk = Hk + torch.diag_embed(alpha * pinv(x.abs()))
-            d = batch_cholesky_solve(gradF.neg(), Hk)
+        # compute step direction
+        diag = (alpha / xmag).masked_fill(is_zero, 0)
+        rhs = (grad + diag*x).masked_fill(is_zero, 0)
+        H = bfgs.H.masked_fill((is_zero.unsqueeze(1) | is_zero.unsqueeze(2)), 0.)
+        H.diagonal(dim1=1, dim2=2).add_(diag + tikhonov)
+        d = batch_cholesky_solve(rhs, H)
 
         # optional strong-wolfe line search
         if line_search:
-            gtd = torch.sum(gradF * d)
-            _, _, t, ls_nevals = \
-                _strong_wolfe(dir_evaluate, x.flatten(), t, d.flatten(), Fval,
-                              gradF.flatten(), gtd)
-            nfev += ls_nevals
+            def line_obj(tt):
+                x_new = x.add(d, alpha=-tt)
+                return float(f(x_new) + alpha * x_new.norm(p=1))
+            t = minimize_scalar(line_obj, bounds=(0,10), method='bounded').x
 
-        # update variables and gradient
-        x_new = x + t * d
-        _, Fval_new, grad_new, gradF_new = evaluate(x_new)
-        nfev += 1
-
-        if disp > 1:
-            print('iter %3d - loss: %0.4f' % (k, Fval_new))
-        if return_losses:
-            losses.append(Fval_new.item())
-
-        # update \delta x and \delta f'(x)
-        s = x_new - x
-        y = grad_new - grad
-        # now update current state
+        # update x
+        x_new = torch.where(is_zero, x, x.add(d, alpha=-t))
+        torch.norm(x_new - x, p=2, out=delta_x)
         x = x_new
-        grad = grad_new
-        Fval = Fval_new
+
+        # re-evaluate f and grad
+        fval, grad = evaluate(x)
+        if verbose > 1:
+            print('iter %3d - loss: %0.4f - dx: %0.4e' % (k, fval, delta_x))
 
         # stopping check
-        gradF_norm = gradF.norm(normp)
-        if gradF_norm <= gtol:
-            return terminate(0, _status_message['success'])
-        if Fval.isinf() or Fval.isnan():
-            return terminate(2, _status_message['pr_loss'])
+        if (delta_x <= xtol) | ~fval.isfinite():
+            break
 
-        # update the BFGS hessian approximation
-        rho_inv = y.mul(s).sum(1, keepdim=True)
-        invalid = rho_inv.abs().le(1e-10)
-        rho = rho_inv.reciprocal().masked_fill(invalid, 1000.)
+        # update hessian estimate
+        bfgs.update(x, grad)
+        t = lr
 
-        Hs = torch.bmm(H, s.unsqueeze(-1))
-        H = torch.where(
-            invalid.unsqueeze(-1),
-            H,
-            torch.addcdiv(
-                torch.baddbmm(H, (rho*y).unsqueeze(-1), y.unsqueeze(-2)),  # H - rho * y @ y^T
-                torch.bmm(Hs, Hs.transpose(-1,-2)),  # Hs @ (Hs)^T
-                torch.bmm(s.unsqueeze(-2), Hs),  # s^T @ Hs
-                value=-1
-            )
-        )
+    if verbose:
+        print("         Current function value: %f" % fval)
+        print("         Iterations: %d" % k)
 
-    # final sanity check
-    if gradF_norm.isnan() or Fval.isnan() or x.isnan().any():
-        return terminate(3, _status_message['nan'])
-
-    # if we get to the end, the maximum num. iterations was reached
-    return terminate(1, "Warning: " + _status_message['maxiter'])
+    return x
